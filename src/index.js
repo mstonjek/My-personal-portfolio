ccc2f40942c1af458aa3d4fb42b451e62572f0f5
Slepicak007 <miggyczech@gmail.com>
2022-11-06 17:28:54 +0100

    install new files, classify code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { ReactDOM } from 'react';
+import ReactDOM from 'react-dom';
 
 import App from './App';
-import '.index.css';
+import './index.css';
 
-ReactDOM.render( <App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+     );
 
