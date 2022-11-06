e41c71271f5b435808bb5735726becabad73c58b
Slepicak007 <miggyczech@gmail.com>
2022-11-06 18:21:56 +0100

    export fixed

--- src/container/index.js
+++ src/container/index.js
@@ -1,6 +1,6 @@
-export { default as About} from './About/About.jsx.js';
-export { default as Footer} from './Footer/Footer.jsx';
-export { default as Header} from './Header/Header.jsx';
-export { default as Skill} from './Skill/Skill.jsx';
-export { default as Testimonial} from './Testimonial/Testimonial.jsx';
-export { default as Work} from './Work/Work.jsx';
+export { default as About} from './About/About';
+export { default as Footer} from './Footer/Footer';
+export { default as Header} from './Header/Header';
+export { default as Skill} from './Skill/Skill';
+export { default as Testimonial} from './Testimonial/Testimonial';
+export { default as Work} from './Work/Work';
