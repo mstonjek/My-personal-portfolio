c33ea2279b4cdb0b73de62c399a3724f6af595c9
Slepicak007 <miggyczech@gmail.com>
2022-11-09 20:53:13 +0100

    sanity functions setuped

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -3,31 +3,25 @@ import { AnimatePresence, motion } from "framer-motion/dist/framer-motion"; // !
 
 import { images } from "../../constants";
 import "../Css/About.css";
+import { urlFor, client } from '../../client';
 
-const abouts = [
-  {
-    title: "Web Development",
-    description: "I am a good web developer.",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Web Desing",
-    description: "I am a good web developer.",
-    imgUrl: images.about02,
-  },
-  {
-    title: "Web Fronted",
-    description: "I am a good web developer.",
-    imgUrl: images.about03,
-  },
-  {
-    title: "Web Beckend",
-    description: "I am a good web developer.",
-    imgUrl: images.about04,
-  },
-];
+
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+    
+    useEffect(() => {
+
+      const query = '*[_type == "abouts"]';
+  
+      client.fetch(query)
+      .then((data) => setAbouts(data))
+  
+    }, []);
+  
+  
+
   return (
     <>
       <h2 className="head-text">
@@ -45,7 +39,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
 
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
