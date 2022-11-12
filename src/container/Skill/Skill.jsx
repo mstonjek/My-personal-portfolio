c6152ac62aba493893c2ca8ac02f70f514f848e0
Slepicak007 <miggyczech@gmail.com>
2022-11-12 18:50:12 +0100

    skills in progress

--- src/container/Skill/Skill.jsx
+++ src/container/Skill/Skill.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion/dist/framer-motion"; // ! better import
+import ReacTooltip from 'react-tooltip';
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 
-import '../Css/Skill.css';
+import "../Css/Skill.css";
+
+
+
 const Skill = () => {
-  return (
-    <div>Skill</div>
-  )
-}
 
-export default Skill
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query).then((data) => {
+      setExperience(data);
+      
+    });
+
+    client.fetch(skillsQuery).then((data) => {
+      setExperience(data);
+    });
+
+  }, []);
+
+return (
+  <>
+  <h2 className="head-text">Skills & Experience</h2>
+
+  <div className="app__skills-container">
+    <motion.div className="app__skills-container">
+
+    </motion.div>
+  </div>
+  
+  </>
+
+)
+  
+};
+
+export default Skill;
