a8eb371599b5662c46d35ab20dded0633db60c5a
Michal Stonjek <mich.stonjek@gmail.com>
2023-10-12 20:23:01 +0200

    build

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -42,49 +42,68 @@ const Footer = () => {
 
   return (
     <>
-      <h2 className="head-text">☕ Brewing Connections Over Coffee! ☕</h2>
+      <div className="app__flex__Contact">
+        <h2 className="head-text">☕ Brewing Connections Over Coffee! ☕</h2>
+        <div className="app__flex">
+          <h3>
+            <a class="icon-link link_color" href="#">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-envelope-plus"
+                viewBox="0 0 16 16"
+              >
+                <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2H2Zm3.708 6.208L1 11.105V5.383l4.708 2.825ZM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2-7-4.2Z" />
+                <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-3.5-2a.5.5 0 0 0-.5.5v1h-1a.5.5 0 0 0 0 1h1v1a.5.5 0 0 0 1 0v-1h1a.5.5 0 0 0 0-1h-1v-1a.5.5 0 0 0-.5-.5Z" />
+              </svg>
+              business@mstonjek.cz
+            </a>
+          </h3>
+        </div>
 
-      
-      {!isFormSubmitted ? (
-        <div className="app__footer-form app__flex">
-          <div className="app__flex">
-            <input
-              className="p-text"
-              type="text"
-              placeholder="Full name"
-              name="username"
-              value={username}
-              onChange={handleChangeInput}
-            />
+        {!isFormSubmitted ? (
+          <div className="app__footer-form app__flex">
+            <div className="app__flex">
+              <input
+                className="p-text"
+                type="text"
+                placeholder="Full name"
+                name="username"
+                value={username}
+                onChange={handleChangeInput}
+              />
+            </div>
+            <div className="app__flex">
+              <input
+                className="p-text"
+                type="email"
+                placeholder="Example@email.com"
+                name="email"
+                value={email}
+                onChange={handleChangeInput}
+              />
+            </div>
+            <div>
+              <textarea
+                className="p-text"
+                placeholder="Write your message"
+                value={message}
+                name="message"
+                onChange={handleChangeInput}
+              />
+            </div>
+            <button type="button" className="p-text" onClick={handleSubmit}>
+              {!loading ? "Send Message" : "Sending..."}
+            </button>
           </div>
-          <div className="app__flex">
-            <input
-              className="p-text"
-              type="email"
-              placeholder="Example@email.com"
-              name="email"
-              value={email}
-              onChange={handleChangeInput}
-            />
-          </div>
+        ) : (
           <div>
-            <textarea
-              className="p-text"
-              placeholder="Write your message"
-              value={message}
-              name="message"
-              onChange={handleChangeInput}
-            />
+            <h3 className="head-text">Thank you for getting in touch!</h3>
           </div>
-          <button type="button" className="p-text" onClick={handleSubmit}>
-            {!loading ? "Send Message" : "Sending..."}
-          </button>
-        </div>
-      ) : (
-        <div>
-          <h3 className="head-text">Thank you for getting in touch!</h3>
-        </div>
-      )}
+        )}
+      </div>
     </>
   );
 };
