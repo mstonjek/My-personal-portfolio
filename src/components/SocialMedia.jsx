c3c26791426e2c45250092c860baff05559fa970
Michal Stonjek <mich.stonjek@gmail.com>
2023-10-12 21:40:57 +0200

    links updated

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -7,19 +7,19 @@ const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <a href="https://instagram.com/your_instagram_username">
+        <a href="https://instagram.com/m.stonjek">
           <BsInstagram />
         </a>
       </div>
 
       <div>
-        <a href="https://linktr.ee/your_username">
+        <a href="https://linktr.ee/m.stonjek">
           <AiOutlineLink />
         </a>
       </div>
 
       <div>
-        <a href="https://github.com/your_github_username">
+        <a href="https://github.com/mstonjek">
           <BsGithub />
         </a>
       </div>
